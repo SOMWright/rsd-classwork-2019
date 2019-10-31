68589070d6bbe3648665de54d749b10ec1c932f7
Sam Wright <SOMWright@users.noreply.github.com>
2019-10-31 11:36:19 +0000

    Add additional unit test for overlap, Answers UCL-RITS/rsd-classwork-2019#25

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -8,3 +8,9 @@ def test_given_input():
     result = overlap_time(short, large)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+    
+def test_class_time():
+    first = time_range("2019-10-31 10:00:00", "2019-10-31 13:00:00")
+    second = time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 600)    
+    result = overlap_time(first, second)
+    assert result == second
